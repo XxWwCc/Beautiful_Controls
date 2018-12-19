6f387e50fabf4abf6276ba8476aac7c63c175c7b
smoker4 <somker4@qi-cloud.com>
2018-12-19 17:05:42 +0800

    first commit

---
+++ app/src/main/java/com/qcloud/myapplication/FruitBean.kt
@@ -0,0 +1,15 @@
+package com.qcloud.myapplication
+
+/**
+ * Description：
+ * author：Smoker
+ * 2018/12/3 17:51
+ */
+class FruitBean {
+    var name = ""
+    var image = 0
+
+    override fun toString(): String {
+        return "FruitBean(name='$name', image='$image')"
+    }
+}
